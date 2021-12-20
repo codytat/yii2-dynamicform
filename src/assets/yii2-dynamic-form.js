4f252da81e82c3f6f64798f8112a1cef5bfa4a52
codytat <33218234+codytat@users.noreply.github.com>
2021-12-20 18:18:08 +0800

    Update to support krajee select2 and fileinput

--- src/assets/yii2-dynamic-form.js
+++ src/assets/yii2-dynamic-form.js
@@ -5,7 +5,7 @@
  *
  * @author Wanderson Bragança <wanderson.wbc@gmail.com>
  */
-(function ($) {
+ (function ($) {
     var pluginName = 'yiiDynamicForm';
 
     var regexID = /^(.+?)([-\d-]{1,})(.+)$/i;
@@ -208,7 +208,9 @@
 
                     widgetsOptions = widgetsOptions.reverse();
                     for (var i = identifiers.length - 1; i >= 1; i--) {
-                        identifiers[i] = $elem.closest(widgetsOptions[i].widgetItem).index();
+                        if(typeof widgetsOptions[i] !== 'undefined'){
+                            identifiers[i] = $elem.closest(widgetsOptions[i].widgetItem).index();
+                        }
                     }
                 }
 
@@ -455,13 +457,9 @@
                     _restoreKrajeeDepdrop($(this));
                 }
 
-                $.when($('#' + id).select2(configSelect2)).done(initSelect2Loading(id, '.select2-container--krajee'));
+                $.when($('#' + id).select2(configSelect2)).done(initS2Loading(id, '.select2-container--krajee'));
 
                 var kvClose = 'kv_close_' + id.replace(/\-/g, '_');
-
-                $('#' + id).on('select2:opening', function(ev) {
-                    initSelect2DropStyle(id, kvClose, ev);
-                });
 
                 $('#' + id).on('select2:unselect', function() {
                     window[kvClose] = true;
